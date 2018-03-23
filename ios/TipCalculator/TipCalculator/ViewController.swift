ee456252e4891575a5877be3541c5ebf644505a8
Nefari0uss <nnefari0uss@gmail.com>
2018-03-22 22:27:23 -0400

    ios native tip calculator project created

---
+++ ios/TipCalculator/TipCalculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Nikit Malkan on 3/22/18.
+//  Copyright © 2018 nefari0uss apps. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
